384cc55aca987b33ae88e9f878eaf0b4728ddd22
Nadav Kaufman <nadav1252@gmail.com>
2025-11-15 17:21:20 +0200

    hw3 f

--- Algebra.java
+++ Algebra.java
@@ -6,16 +6,16 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+	    System.out.println(plus(3,-2));   // 2 + 3
+	    System.out.println(minus(7,-2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
+ 		System.out.println(times(-4,2));  // 3 * 4
+   		System.out.println(plus(2,times(-4,2)));  // 2 + 4 * 2
+   		System.out.println(pow(-5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+   		System.out.println(div(-12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
+   		System.out.println(div(21,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
    		System.out.println(sqrt(36));
@@ -26,32 +26,82 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2)
 	 {
+		if(x2>=0)
+		{
 		for (int i = 0; i <x2; i++)
 		{
 		x1++;
 	    }
 		return x1;
       } 
+	  else
+	  {
+		for (int i = 0; i <-x2; i++)
+		{
+		x1--;
+	    }
+		return x1;
+	  }
+	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) 
 	{
-        for (int i = 0; i <x2; i++)
+       if(x2>=0)
+		{
+		for (int i = 0; i <x2; i++)
 		{
 	    x1--;
 		}
 	    return x1;
+	    }
+		else
+	  {
+		for (int i = 0; i <-x2; i++)
+		{
+		x1++;
+	    }
+		return x1;
+	  } 
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		 int res = 0;
+	    if( (x1>=0  && x2>=0 ))
+		{
 		for (int i = 1; i <= x2; i++)
 		{
 		
 	    res = plus(res,x1);
 		}
-	    return res;
+		return res;
+	    }
+		if( (x1<0  && x2>=0 ))
+		{
+		for (int i = 1; i <= x2; i++)
+		{
+		
+	    res = plus(res,x1);
+		}
+		return res;
+	    }
+		if( (x1>=0  && x2<0 ))
+		{
+		for (int i = 1; i <= -x2; i++)
+		{
+		
+	    res = plus(res,x1);
+		}
+		return -res;
+	    }
+		for (int i = 1; i <= -x2; i++)
+		{
+		
+	    res = plus(res,x1);
+		}
+		return res;
+	   
 	}
 
 	// Returns x^n (for n >= 0)
@@ -61,32 +111,45 @@ public class Algebra {
 		{
 		 res= times(res,x);
 		}
-		return res;
+		if( n % 2 == 0)
+		{
+		return Math.abs(res);
+		}
+		else
+		{
+		if(x<0)
+		{
+		return -Math.abs(res);
+		}
+		return Math.abs(res);
+	}
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-       int s = x2, count = 0;
-		while((x1 >= s))
+       int s = Math.abs(x2), count = 0;
+		while((Math.abs(x1) >= s))
 		{
-		s = plus(s,x2);
+		s = plus(s,Math.abs(x2));
 		count++;
 	   }
+	   if((x1 >= 0 && x2 >= 0)|| (x1 >= 0 && x2 >= 0)){
    		return count;
+	   }
+	   return -count ;
+	
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		int q = div(x1,x2);
-		int n = times(q,x2);
-		return x1-x2;
+		int q = Math.abs(div(x1,x2));
+		int n = Math.abs(times(q,x2));
+		return x1-n;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		int g = 0;
-		boolean b = times(g,g) < x, c = times(g,g) == x;
-		
 		while(times(g,g) <= x)
 		{
 		 g++;
